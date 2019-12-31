3125f9b952c0dd17ebecac4990ffe2c4b2a08288
mzw <517695099@qq.com>
2019-12-31 14:47:56 +0800

    this is init git

--- src/main/java/mzw/TestgitApplication.java
+++ src/main/java/mzw/TestgitApplication.java
@@ -9,4 +9,8 @@ public class TestgitApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestgitApplication.class, args);
     }
+
+    public void m1() {
+
+    }
 }
